4f775bfa35bf05d8f0ae24f32d26fe8252b2e054
Lakshantha Dissanayake <lakshanthad@yahoo.com>
2020-11-17 17:11:42 +0530

    Update connect_network.ino

--- Classroom 2/connect_network/connect_network.ino
+++ Classroom 2/connect_network/connect_network.ino
@@ -1,7 +1,7 @@
 #include "rpcWiFi.h" //include Wi-Fi library 
  
-const char* ssid = ""; //Wi-Fi name 
-const char* password =  ""; //Wi-Fi password 
+const char* ssid = "Enter_WiFI_name"; //Wi-Fi name 
+const char* password =  "Enter_WiFi_password"; //Wi-Fi password 
  
 void setup() {
     Serial.begin(115200); //start serial communication
