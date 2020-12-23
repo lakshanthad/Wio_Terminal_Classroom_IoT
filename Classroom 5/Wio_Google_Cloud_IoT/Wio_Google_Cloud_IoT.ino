8f22f97e0ae653bbececbf5bb4471642680033c6
Lakshantha Dissanayake <lakshanthad@yahoo.com>
2020-12-23 14:58:10 +0530

    Add files via upload

---
+++ Classroom 5/Wio_Google_Cloud_IoT/Wio_Google_Cloud_IoT.ino
@@ -0,0 +1,81 @@
+/******************************************************************************
+ * Copyright 2018 Google
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *****************************************************************************/
+
+#if defined(ARDUINO_SAMD_MKR1000) or defined(ESP8266)
+#define __SKIP_ESP32__
+#endif
+
+#if defined(ESP32) || defined(WIO_TERMINAL)
+#define __ESP32_MQTT_H__
+
+#endif
+
+#ifdef __SKIP_ESP32__
+
+#include <Arduino.h>
+
+void setup(){
+  Serial.begin(115200);
+}
+
+void loop(){
+  Serial.println("Hello World");
+}
+
+#endif
+
+#ifdef __ESP32_MQTT_H__
+#include "esp32-mqtt.h"
+#include "DHT.h" //DHT library
+
+#define DHTPIN 0 //Define Signal Pin of DHT
+#define DHTTYPE DHT11 //Define DHT Sensor Type
+DHT dht(DHTPIN, DHTTYPE); //Initializing DHT sensor  
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  pinMode(LED_BUILTIN, OUTPUT);
+  setupCloudIoT();
+  dht.begin(); 
+}
+
+unsigned long lastMillis = 0;
+void loop() {
+  mqtt->loop();
+  delay(10);  // <- fixes some issues with WiFi stability
+
+  if (!mqttClient->connected()) {
+    connect();
+  }
+
+  // TODO: replace with your code
+  // publish a message roughly every second.
+  if (millis() - lastMillis > 60000) {
+    lastMillis = millis();
+
+    int temperature = dht.readTemperature(); //Assign variable to store temperature
+    int humidity = dht.readHumidity(); //Assign variable to store humidity
+
+    
+    String payload = String("{\"timestamp\":") + getNTPtime() +
+                     String(",\"temperature\":") + temperature +
+                     String(",\"humidity\":") + humidity +
+                     String("}");
+    publishTelemetry(payload);
+    
+  }
+}
+#endif
