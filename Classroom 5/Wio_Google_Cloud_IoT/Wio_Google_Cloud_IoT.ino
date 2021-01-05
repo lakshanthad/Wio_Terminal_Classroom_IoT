001736d9ddbc95d7189c05445223bd39bc20fd15
Lakshantha Dissanayake <lakshanthad@yahoo.com>
2021-01-05 16:10:06 +0530

    Add files via upload

---
+++ Classroom 5/Wio_Google_Cloud_IoT/Wio_Google_Cloud_IoT.ino
@@ -0,0 +1,69 @@
+/******************************************************************************
+ * Copyright 2018 Google
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *****************************************************************************/
+
+#if defined(ARDUINO_SAMD_MKR1000) or defined(ESP8266)
+#define __SKIP_ESP32__
+#endif
+
+#if defined(ESP32) || defined(WIO_TERMINAL)
+#define __ESP32_MQTT_H__
+#endif
+
+#ifdef __SKIP_ESP32__
+
+#include <Arduino.h>
+
+void setup(){
+  Serial.begin(115200);
+}
+
+void loop(){
+  Serial.println("Hello World");
+}
+
+#endif
+
+#ifdef __ESP32_MQTT_H__
+#include "esp32-mqtt.h"
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  pinMode(LED_BUILTIN, OUTPUT);
+  setupCloudIoT();
+}
+
+unsigned long lastMillis = 0;
+void loop() {
+  int light = analogRead(WIO_LIGHT); //assign variable to store light sensor values 
+  light = map(light,0,1023,0,100); //map sensor values
+  
+  mqtt->loop();
+  delay(10);  // <- fixes some issues with WiFi stability
+
+  if (!mqttClient->connected()) {
+    connect();
+  }
+
+  // TODO: replace with your code
+  // publish a message roughly every second.
+  if (millis() - lastMillis > 5000) {
+    lastMillis = millis();
+    //publishTelemetry(mqttClient, "/sensors", getDefaultSensor());
+    publishTelemetry(getDefaultSensor());
+    publishTelemetry("/light",String(light));
+    
+  }
+}
+#endif
